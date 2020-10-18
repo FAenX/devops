9f8577eb49c33bce4334c7c17c4e5381d0ca0d01
manu <emmanuel@touchinspiration.com>
2020-10-19 02:20:07 +0300

    nginx conf generator

--- nginx_conf.py
+++ nginx_conf.py
@@ -1,13 +1,24 @@
 from string import Template
-from templates import proxy_to_localhost_nginx_conf
+import argparse
+from templates import proxy_to_localhost_nginx_conf, static_server_with_proxy_nginx_conf
 
-# nginx file
+# parse
+# terminal argument parser
+def parseArgs():
+    # create a parser
+    parser = argparse.ArgumentParser(description='generate nginx conf.')
+    parser.add_argument("--static", action='store_true', help="static server with proxy_pass.")
+    parser.add_argument("--servername", help="server_name.")
+    parser.add_argument("--proxy", help="proxy_pass usr")
+    parser.add_argument("--port", help="port for app running on local host for proxy server")
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == '__main__': 
-  path = '/etc/nginx/sites-available'
-  port = 3004
-  server_name = 'https://valueads-api.touchinspiration.net'
+  args = parseArgs()
+  server_name = args.servername
+  port = args.port
 
   s=proxy_to_localhost_nginx_conf.safe_substitute(server_name=server_name, port=port)
   print(s)

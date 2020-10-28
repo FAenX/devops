081c0042097424b7c3f2b907268001c12366427b
manu <emmanuel@touchinspiration.com>
2020-10-29 02:53:20 +0300

    fix proxy to localhost config

--- nginx_conf.py
+++ nginx_conf.py
@@ -12,6 +12,7 @@ def parseArgs():
     # create a parser
     parser = argparse.ArgumentParser(description='generate nginx conf.')
     parser.add_argument("--static", action='store_true', help="static server with proxy_pass.")
+    parser.add_argument("--proxy-to-local", action='store_true', help="proxy server to localhost.")
     parser.add_argument("servername", help="server_name.")
     parser.add_argument("--proxy", help="proxy_pass usr")
     parser.add_argument("--port", help="port for app running on local host for proxy server")
@@ -36,12 +37,13 @@ if __name__ == '__main__':
       proxy=proxy, 
       site_name=server_name
       )
+    print(s)
   
   # proxy server conf
-  else:
+  if args.proxy_to_local:
     s=nginx.proxy_to_localhost_nginx_conf.safe_substitute(
       server_name=server_name, 
       port=port
       )
 
-  print(s)
+    print(s)

a9a30d46322806a75fc8d810ee1c4d9d8178187a
manu <emmanuel@touchinspiration.com>
2020-11-17 12:39:53 +0300

    f

--- templates/docker/docker_post_receive.py
+++ templates/docker/docker_post_receive.py
@@ -6,7 +6,7 @@ docker = Template(r'''
     docker build --tag $APP_NAME:1.0 .
     docker stop $APP_NAME
     docker rm $APP_NAME
-    docker run --publish $PORT:3000 --detach --name $APP_NAME $APP_NAME:1.0
+    docker run --publish $PORT:3000 --restart always --detach --name $APP_NAME $APP_NAME:1.0
 ''')
 
 

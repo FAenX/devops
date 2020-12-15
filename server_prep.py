121cc0e787923ff1afd8b98fec76118f35ba8e6f
manu <emmanuel@touchinspiration.com>
2020-12-15 15:26:53 +0300

    server_prep

--- server_prep.py
+++ server_prep.py
@@ -1,6 +1,24 @@
-# apt-get update
-# apt-get install nginx
+import os
+import sys
+import subprocess
+import argparse
+import shutil
+import getpass
 
-# curl -fsSL https://get.docker.com -o get-docker.sh
-# sudo sh get-docker.sh
+def server_prep(): 
+    sudoPassword = getpass.getpass('sudo password or blank: ')
+    command1 = 'apt update'
+    command2 = 'apt-get install nginx'
+    command3 = 'curl -fsSL https://get.docker.com -o get-docker.sh'
+    command4 = 'sh get-docker.sh'
 
+    os.system('echo %s|sudo -S %s' % (sudoPassword, command1))
+    os.system('echo %s|sudo -S %s' % (sudoPassword, command2))
+    os.system('echo %s|sudo -S %s' % (sudoPassword, command3))
+    os.system('echo %s|sudo -S %s' % (sudoPassword, command4))
+
+
+if __name__ == '__main__':   
+    server_prep()
+
+    # 
--- server_status.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def server_status(url):
-    return requests.get(url)
-
-
-if __name__=='__main__':
-    response = server_status('https://boma-api.touchinspiration.net/login').status_code
-    print(response)

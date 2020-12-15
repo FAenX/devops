5c23f4fd0e6b0d03dd6a20080a9ad468d46fc669
manu <emmanuel@touchinspiration.com>
2020-12-15 16:00:57 +0300

    server_prep

--- server_prep.py
+++ server_prep.py
@@ -5,17 +5,61 @@ import argparse
 import shutil
 import getpass
 
-def server_prep(): 
-    sudoPassword = getpass.getpass('sudo password or blank: ')
+
+def server_prep():     
+
+    process = subprocess.Popen('docker -v', shell=True, stdout=subprocess.PIPE)
+    process.wait()
+    if process.returncode == 0:
+        print('docker already installed')
+    else:
+        installDocker()
+        
+        # os.system('echo %s|sudo -S %s' % (sudoPassword, command4))
+        
+    
     command1 = 'apt update'
     command2 = 'apt-get install nginx'
+
+    c1 = subprocess.Popen('sudo %s' % (command1), shell=True, stdout=subprocess.PIPE)
+    c1.wait()
+
+    c2 = subprocess.Popen('sudo %s' % (command2), shell=True, stdout=subprocess.PIPE)
+    c2.wait()
+    
+
+    if c1.returncode == 0:
+        print('updated')
+    else:
+        print('error updating')
+
+    if c2.returncode == 0:
+        print('nginx installed')
+    else:
+        sys.exit('error installing')
+
+
+
+def installDocker():
     command3 = 'curl -fsSL https://get.docker.com -o get-docker.sh'
     command4 = 'sh get-docker.sh'
 
-    os.system('echo %s|sudo -S %s' % (sudoPassword, command1))
-    os.system('echo %s|sudo -S %s' % (sudoPassword, command2))
-    os.system('echo %s|sudo -S %s' % (sudoPassword, command3))
-    os.system('echo %s|sudo -S %s' % (sudoPassword, command4))
+    c3 = subprocess.Popen('sudo %s' % (command3), shell=True, stdout=subprocess.PIPE)
+    c3.wait()
+
+    c4 = subprocess.Popen('sudo %s' % (command4), shell=True, stdout=subprocess.PIPE)
+    c4.wait()
+
+    if c3.returncode == 0:
+        print('downloaded install script')
+    else:
+        sys.exit('error installing docker')
+
+    if c4.returncode == 0:
+        print('docker installed')
+    else:
+        sys.exit('error installing docker')
+    
 
 
 if __name__ == '__main__':   

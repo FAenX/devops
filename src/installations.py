56ad03b88a0ca9620e28344917be24032782c42d
Emmanuel <emmanuel.davidson@thejitu.com>
2022-08-20 18:22:59 +0300

    excecutable

--- src/installations.py
+++ src/installations.py
@@ -6,8 +6,8 @@ import os
 
 def install_docker():
     with subprocess.Popen(f'\
-    curl -fsSL https://get.docker.com -o .get-docker.sh \
-    && sh .get-docker.sh \
+    apt install -y docker.io\
+    && apt install docker-compose -y \
     ', 
     shell=True, stdout=subprocess.PIPE) as proc:        
         for line in proc.stdout:
@@ -16,7 +16,8 @@ def install_docker():
 def install_wordpress():
     install_docker()
     with subprocess.Popen('\
-        docker-compose up -d -f dockerfiles/wordpress.yml\
+        ls -al && \
+        docker-compose -f src/dockerfiles/wordpress.yml up -d\
     ', 
     shell=True, stdout=subprocess.PIPE) as proc:        
         for line in proc.stdout:

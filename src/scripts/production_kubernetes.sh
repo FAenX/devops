5b805aff011b24199fc57227411b880dcc16662a
Emmanuel <emmanuel.davidson@thejitu.com>
2022-09-18 17:11:35 +0300

    break it

--- src/scripts/production_kubernetes.sh
+++ src/scripts/production_kubernetes.sh
@@ -1,49 +1,27 @@
 #! /bin/bash
 
-{
 hostnamectl set-hostname "127.0.0.1" 
 exec bash
-}
 
-||{ echo "Error" ; exit 1; }
 
-{
 
 swapoff -a
 sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-}
 
-||{ echo "Error" ; exit 1; }
-
-{
 tee /etc/modules-load.d/containerd.conf <<EOF
 overlay
 br_netfilter
 EOF
-
-}
-
-||{ echo "Error" ; exit 1; }
-
 modprobe overlay
 modprobe br_netfilter
 
-{
 tee /etc/sysctl.d/kubernetes.conf <<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF 
 
-}
-
-||{ echo "Error" ; exit 1; }
-
-{
-
 sysctl --system
-    
-} ||{ echo "Error" ; exit 1; }
 
 apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
@@ -63,5 +41,3 @@ apt-mark hold kubelet kubeadm kubectl
 kubeadm init --control-plane-endpoint='droplet ip'
 curl https://projectcalico.docs.tigera.io/manifests/calico.yaml -O
 kubectl apply -f calico.yaml
-    
-    

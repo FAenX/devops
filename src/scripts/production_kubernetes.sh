41ca0cc8d3cb52db105d7a2bea0b26834bfb9fc3
Emmanuel <emmanuel.davidson@thejitu.com>
2022-09-18 18:36:46 +0300

    test remotely

--- src/scripts/production_kubernetes.sh
+++ src/scripts/production_kubernetes.sh
@@ -25,21 +25,26 @@ tee /etc/modules-load.d/containerd.conf <<EOF
 overlay
 br_netfilter
 EOF
+
+modprobe overlay
+modprobe br_netfilter
 } || {
-echo "Error: tee"
+echo "Error: tee /etc/modules-load.d/containerd.conf"
+}
+
+{
+tee /etc/sysctl.d/kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sysctl --system
+} || {
+echo "Error: tee /etc/sysctl.d/kubernetes.conf"
 }
 
 
-# modprobe overlay
-# modprobe br_netfilter
-
-# tee /etc/sysctl.d/kubernetes.conf <<EOF
-# net.bridge.bridge-nf-call-ip6tables = 1
-# net.bridge.bridge-nf-call-iptables = 1
-# net.ipv4.ip_forward = 1
-# EOF 
-
-# sysctl --system
 
 # apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
 # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg

eca114ae5f8fee615ebd91c902f5424c326cd7cb
Emmanuel <emmanuel.davidson@thejitu.com>
2022-09-19 22:27:18 +0300

    script udates

--- src/scripts/production_kubernetes.sh
+++ src/scripts/production_kubernetes.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-# apt update
-# apt install curl -y
+apt update
 
 ips=$(hostname -I)
 echo $ips
@@ -15,25 +14,17 @@ echo "Detected IP:  ${iparr[0]} "
 echo "setting hostname to ${iparr[0]}"
 hostnamectl set-hostname ${iparr[0]} || echo "Failed to set hostname to IP"
 
-
-
-# exec bash to to apply hostname change
-exec bash
-
-
 # disable swap
 echo "Disabling swap"
-swapoff -a
-sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+swapoff -a && sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab || echo "Failed to disable swap"
+
 
 tee /etc/modules-load.d/containerd.conf <<EOF
 overlay
 br_netfilter
 EOF
 
-
-modprobe overlay
-modprobe br_netfilter
+modprobe overlay && modprobe br_netfilter || echo "Failed to load modules"
 
 # 
 tee /etc/sysctl.d/kubernetes.conf <<EOF
@@ -46,21 +37,12 @@ sysctl --system
 
 
 
-# apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates  || echo "Failed to install packages"
 
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+(curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg) \
+&& add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+|| (echo "Failed to add docker repo" && exit 1)
 
-# ||
-# echo "Error: curl -fsSL https://download.docker.com/linux/ubuntu/gpg"
-# exit 1
-
-
-
-
-# add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-# ||
-# echo "Error: curl -fsSL https://download.docker.com/linux/ubuntu/gpg"
-# exit 1
 
 
 
@@ -72,19 +54,12 @@ sysctl --system
 # containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
 # systemctl restart containerd
 # systemctl enable containerd
-# ||
-# echo "Error: apt install -y containerd.io failed"
-# echo "exiting"
-# exit 1
 
 
 
 # curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 # apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-# ||
-# echo "Error: curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -"
-# echo "exiting"
-# exit 1
+
 
 
 # apt update
@@ -92,24 +67,16 @@ sysctl --system
 
 # apt install -y kubelet kubeadm kubectl
 # apt-mark hold kubelet kubeadm kubectl
-# ||
-# echo "Error: apt install -y kubelet kubeadm kubectl"
-# echo "exiting"
-# exit 1
 
 
 
-
-# kubeadm init --control-plane-endpoint=$ip 
+# kubeadm init --control-plane-endpoint=$ip  || echo "Error: failed to init kubeadm"
 # export KUBECONFIG=/etc/kubernetes/admin.conf
 
 
 # curl https://projectcalico.docs.tigera.io/manifests/calico.yaml -O
 # kubectl apply -f calico.yaml
 
-# ||
-# echo "Error: kubeadm init --control-plane-endpoint=$ip"
-# echo "exiting"
-# exit 1
+
 
 

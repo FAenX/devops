2e2ca1ccccc5e3102ce6ca3b6a13b7acd02c0115
Emmanuel <emmanuel.davidson@thejitu.com>
2022-09-19 20:04:03 +0300

    add kubernetes installation script

--- src/scripts/production_kubernetes.sh
+++ src/scripts/production_kubernetes.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+apt update
+apt install curl
+
 {
 hostnamectl set-hostname "127.0.0.1" 
 # exec bash
@@ -44,23 +47,37 @@ sysctl --system
 echo "Error: tee /etc/sysctl.d/kubernetes.conf"
 }
 
+apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+
+{
+{
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+
+}||{
+echo "Error: curl -fsSL https://download.docker.com/linux/ubuntu/gpg"
+exit 1
+}
+
+{
+
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+}||{
+echo "Error: curl -fsSL https://download.docker.com/linux/ubuntu/gpg"
+}
+exit 1
+}
 
 
-# apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
-
-# add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-# apt update
-# apt install -y containerd.io
-# containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
-# systemctl restart containerd
-# systemctl enable containerd
-# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-# apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-# apt update
-# apt install -y kubelet kubeadm kubectl
-# apt-mark hold kubelet kubeadm kubectl
-# kubeadm init --control-plane-endpoint='droplet ip'
-# curl https://projectcalico.docs.tigera.io/manifests/calico.yaml -O
-# kubectl apply -f calico.yaml
+apt update
+apt install -y containerd.io
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+systemctl restart containerd
+systemctl enable containerd
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+apt update
+apt install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+kubeadm init --control-plane-endpoint='droplet ip'
+curl https://projectcalico.docs.tigera.io/manifests/calico.yaml -O
+kubectl apply -f calico.yaml

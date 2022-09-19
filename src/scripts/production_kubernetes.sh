1866e1939d3ee5ac916884bc7420a3cb46eb62b5
Emmanuel <emmanuel.davidson@thejitu.com>
2022-09-19 20:13:34 +0300

    add kubernetes installation script

--- src/scripts/production_kubernetes.sh
+++ src/scripts/production_kubernetes.sh
@@ -63,9 +63,10 @@ exit 1
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 }||{
 echo "Error: curl -fsSL https://download.docker.com/linux/ubuntu/gpg"
-}
 exit 1
 }
+
+
 
 
 apt update

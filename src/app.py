a65493e14259bcf5afb9dccc3fb52faeb207cee0
Emmanuel <emmanuel.davidson@thejitu.com>
2022-09-25 15:33:34 +0300

    staggering through

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-server = Flask(__name__)
+app = Flask(__name__)
 
-@server.route("/")
+@app.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-   server.run(host='0.0.0.0')
+   app.run(host='0.0.0.0')

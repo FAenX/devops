e0141995eaf0f189fe865540970c3fef3b0c8798
Emmanuel <emmanuel.davidson@thejitu.com>
2022-09-27 21:05:30 +0300

    test with master hostname as kube-master

--- src/scripts/production.sh
+++ src/scripts/production.sh
@@ -73,7 +73,7 @@ apt update
 apt install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
 
-kubeadm init --control-plane-endpoint='kube-master' --ignore-preflight-errors=... -v=9 || echo "Error: failed to init kubeadm"
+kubeadm init --control-plane-endpoint='kube-master' --ignore-preflight-errors=all -v=9 || echo "Error: failed to init kubeadm"
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
 

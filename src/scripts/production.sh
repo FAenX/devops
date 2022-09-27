549d1c5885442f71be6a3d25edb3bbf28a1bddf8
Emmanuel <emmanuel.davidson@thejitu.com>
2022-09-27 21:20:32 +0300

    test with master hostname as kube-master

--- src/scripts/production.sh
+++ src/scripts/production.sh
@@ -51,20 +51,20 @@ sysctl --system
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg \
 && add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
-|| (echo "Failed to add docker repo" && exit 1)
+||  exit 1
 
 apt update
 
-apt install -y containerd.io containerd docker.io
+apt install -y containerd.io containerd docker.io \
 && containerd config default | tee /etc/containerd/config.toml >/dev/null 2>&1 \
 && systemctl restart containerd \
 && systemctl enable containerd || echo "Failed to install containerd" 
 
 
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-|| (echo "Failed to add kubernetes repo" && exit 1)
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - \
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" \
+||  exit 1
 
 
 apt update

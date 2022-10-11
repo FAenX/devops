5f6ee3f970f37d73bac8000d30b1ff248bdb0608
Emmanuel <emmanuel.davidson@thejitu.com>
2022-10-11 22:26:00 +0300

    react docker file

--- src/git/react_post_receive.py
+++ src/git/react_post_receive.py
@@ -14,21 +14,47 @@ def create_react_post_receive(project_name):
     subprocess.check_call(f'git init --bare --shared=all {git_dir_path}', shell=True)
 
     template = f'''
-    # create a post-receive file
-    #!/bin/bash
-    # generated automatically
+# create a post-receive file
+#!/bin/bash
+# generated automatically
 
-    # Deploy the content to the temporary directory
-    git --work-tree={tempdir} --git-dir={git_dir_path} checkout -f || exit
+# Deploy the content to the temporary directory
+mkdir -p {tempdir}
+mkdir -p {project_path}
+git --work-tree={tempdir} --git-dir={git_dir_path} checkout -f || exit;
 
-    # Replace the content of the production directory
-    cd {project_path} || exit
-    pwd
-    rm -rf .\/* || exit
-    mv {tempdir}\/* {project_path} || exit
-    npm install
-    npm run build
-    '''  
+rm -rf {project_path}/*
+cp -r {tempdir}/* {project_path} || exit;
+rm -rf {tempdir} || exit;
+cd {project_path} || exit;
+
+# create a Dockerfile 
+# add multiline string to a file
+cat <<EOF > {project_path}/Dockerfile
+# Name the node stage "builder"
+FROM node:10 AS builder
+# Set working directory
+WORKDIR /app
+# Copy all files from current directory to working dir in image
+COPY . .
+# install node modules and build assets
+RUN npm install && npm run build
+
+# nginx state for serving content
+FROM nginx:alpine
+# Set working directory to nginx asset directory
+WORKDIR /usr/share/nginx/html
+# Remove default nginx static assets
+RUN rm -rf ./*
+# Copy static assets from builder stage
+COPY --from=builder /app/build .
+# Containers run nginx with global directives and daemon off
+ENTRYPOINT ["nginx", "-g", "daemon off;"]
+EOF
+
+docker build -t react-nginx .;
+docker run -d -p 3002:80 react-nginx --name {project_name};
+'''  
 
     with open(f'{git_dir_path}/hooks/post-receive', 'w') as file:
         file.write(template)
